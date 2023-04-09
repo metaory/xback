dc0a526a28b437b519311b55a6d61e3ad423e9c3
metaory <metaory@gmail.com>
2023-04-09 19:57:59 +0800

    refactors

--- util.sh
+++ util.sh
@@ -14,6 +14,15 @@ header() {
 EOF
 }
 
+catches() {
+	# Comment lines
+	if [[ "${1:0:1}" == "#" ]]; then echo 1; fi
+	# Empty lines
+	if [[ -z $1 ]]; then echo 2; fi
+	# Exists
+	if ! [[ -e ${2:-$1} ]]; then echo 3; fi
+}
+
 # deserialize() {
 # 	out="${1/#.\/data\//}"
 # 	echo "${dest//__/\/}"
