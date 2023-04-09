0e9d548bf3a45d023612082c1146518b4583dc84
metaory <metaory@gmail.com>
2023-04-09 22:06:14 +0800

    bugfix

--- util.sh
+++ util.sh
@@ -15,12 +15,14 @@ EOF
 }
 
 catches() {
-	# Comment lines
-	if [[ "${1:0:1}" == "#" ]]; then echo 1; fi
-	# Empty lines
-	if [[ -z $1 ]]; then echo 2; fi
 	# Exists
-	if ! [[ -e ${2:-$1} ]]; then echo 3; fi
+	if ! [[ -e ${2:-$1} ]]; then return 3; fi
+	# Empty lines
+	if [[ -z $1 ]]; then return 2; fi
+	# Comment lines
+	if [[ "${1:0:1}" == "#" ]]; then return 1; fi
+	# Default
+	return 0
 }
 
 # deserialize() {
